e74d203147c756d929700531edca1b5cbcd527f3
Aman Velpula <velpmail@gmail.com>
2023-01-21 14:58:11 -0500

    creating blob function in JS

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ selectMenu = document.querySelector(".save-as select")
 saveBtn = document.querySelector(".save-btn")
 
 saveBtn.addEventListener("click", () => {
-    console.log(textarea.value);
+    const blob = new Blob([textarea.value], {type: selectMenu.value});
+    const fileUrl = URL.createObjectURL(blob);
+    const link = document.createElement("a"); //creating <a> tag;
 })
