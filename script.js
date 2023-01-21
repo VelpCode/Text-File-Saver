d300b81c75d2477fa7e2bea43c1a035f7ccfc6ef
Aman Velpula <velpmail@gmail.com>
2023-01-21 15:04:28 -0500

    Finalizing script.js

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ fileNameInput = document.querySelector(".file-name input")
 selectMenu = document.querySelector(".save-as select")
 saveBtn = document.querySelector(".save-btn")
 
+selectMenu.addEventListener("change", () => {
+    let selectedOption = selectMenu.options[selectMenu.selectedIndex].text;
+    saveBtn.innerText = `Save As ${selectedOption.split(" ")[0]} File`
+})
+
 saveBtn.addEventListener("click", () => {
     const blob = new Blob([textarea.value], {type: selectMenu.value});
     const fileUrl = URL.createObjectURL(blob);
     const link = document.createElement("a"); //creating <a> tag;
+    link.download = fileNameInput.value;
+    link.href = fileUrl;
+    link.click();
 })
